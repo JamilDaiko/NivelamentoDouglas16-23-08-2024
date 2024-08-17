1ad9bca42b4d61929033c328ac327652155307d8
JamilDaiko <161781470+JamilDaiko@users.noreply.github.com>
2024-08-16 21:24:18 -0300

    calculo

--- index.java
+++ index.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class index {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe as horas : ");
+        int horas = sc.nextInt();
+        System.out.println("Informe as minutos: ");
+        int minutos = sc.nextInt();
+        System.out.println("Informe as segundos: ");
+        int segundos = sc.nextInt();
+
+        int resultado;
+        horas= horas*60*60;
+        minutos= minutos*60;
+        resultado= horas+minutos+segundos;
+
+        System.out.println("Segundos: " +resultado);
     }
 }
